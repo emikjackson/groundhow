4df87f5aa6d7d3531d1661c40a9a90bfed792574
Emi Jackson <emij@umich.edu>
2017-04-18 01:50:51 -0400

    removed erroneous secret line 61

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(methodOverride('_method'));
-app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+//app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
